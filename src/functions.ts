83a0d4a4a239d4d467411b13b645eb77d748005f
Shadrick Siwakwi <shadrick.siwakwi@zamtel.co.zm>
2021-12-06 02:35:45 +0200

    lambda function to to add a To-do list item tested and working as correctly(request data not yet validated)

--- src/functions.ts
+++ src/functions.ts
@@ -1,15 +1,37 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import AWS from 'aws-sdk'
+import { v4 } from 'uuid'
 
-export const hello = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+
+const dbClient = new AWS.DynamoDB.DocumentClient()
+
+interface ToDoListI {
+  label: string,
+  completed: boolean,
+}
+
+//This function is responsible for adding a new item to the To-Do List
+export const addNewItem = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const reqData = JSON.parse(event.body as string) as ToDoListI
+
+  const createdAt = new Date
+
+  const updatedAt = new Date
+
+  const listItem = {
+    toDoListID: v4(),
+    ...reqData,
+    createdAt,
+    updatedAt,
+  }
+
+  await dbClient.put({
+    TableName: 'ToDoListTable',
+    Item: listItem,
+  }).promise()
+
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v1.0! Your function executed successfully!",
-        input: event,
-      },
-      null,
-      2,
-    ),
+    body: JSON.stringify((listItem), null, 2),
   };
 };
